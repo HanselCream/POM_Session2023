63a7bc011dc0f65f4c95e063ba8e60dfe74a511a
print ("hans radam") <radamhans15@gmail.com>
2023-05-22 17:59:04 +0800

    commit

--- src/main/java/com/qa/opencart/utils/AppConstants.java
+++ src/main/java/com/qa/opencart/utils/AppConstants.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class AppConstants {
 
-    //TODO: INTEGERS CONSTANT
+    //INTEGERS CONSTANT
     public static final int SHORT_DEFAULT_WAIT = 5; //ALT-F7  to see usage
     public static final int MEDIUM_DEFAULT_WAIT = 10;
     public static final int LONG_DEFAULT_WAIT = 10;
 
     //ShortCut CTRL-N
-    // TODO: Open [LoginPage]
+    // LoginPage
     public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login";
     public static final String LOGIN_PAGE_URL_FRACTION_VALUE = "route=account/login";
 
 
-    //TODO: Open [AccountPage]
+    //AccountPage
     public static final String ACCOUNTS_PAGE_TITLE_VALUE = "My Account";
 
 
@@ -26,8 +26,6 @@ public class AppConstants {
     //LIST CONSTANT
     public static final List<String> EXP_ACCOUNTS_HEADER_LIST =
             Arrays.asList("My Account", "My Orders", "My Affiliate Account", "Newsletter");
-
-
 
 
 
